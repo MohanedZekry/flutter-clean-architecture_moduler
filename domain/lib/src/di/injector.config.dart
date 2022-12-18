009fa151b52e517bf3002b5ff355e125f2324926
MohanedZekry <mohanadzekry@gmail.com>
2022-12-18 14:53:51 +0200

    Finish domain layer 🏁 & inject it 💉

---
+++ domain/lib/src/di/injector.config.dart
@@ -0,0 +1,45 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// InjectableConfigGenerator
+// **************************************************************************
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:domain/domain.dart' as _i8;
+import 'package:domain/src/repository/email_repository.dart' as _i4;
+import 'package:domain/src/use_cases/add_email_use_case.dart' as _i3;
+import 'package:domain/src/use_cases/delete_email_use_case.dart' as _i5;
+import 'package:domain/src/use_cases/get_all_emails_use_case.dart' as _i6;
+import 'package:domain/src/use_cases/get_most_popular_emails_use_case.dart'
+    as _i7;
+import 'package:domain/src/use_cases/update_email_use_case.dart' as _i9;
+import 'package:get_it/get_it.dart' as _i1;
+import 'package:injectable/injectable.dart' as _i2;
+
+/// ignore_for_file: unnecessary_lambdas
+/// ignore_for_file: lines_longer_than_80_chars
+extension GetItInjectableX on _i1.GetIt {
+  /// initializes the registration of main-scope dependencies inside of [GetIt]
+  _i1.GetIt $initDomainGetIt({
+    String? environment,
+    _i2.EnvironmentFilter? environmentFilter,
+  }) {
+    final gh = _i2.GetItHelper(
+      this,
+      environment,
+      environmentFilter,
+    );
+    gh.factory<_i3.AddEmailUseCase>(() =>
+        _i3.AddEmailUseCase(articleRepository: gh<_i4.EmailRepository>()));
+    gh.factory<_i5.DeleteEmailUseCase>(() =>
+        _i5.DeleteEmailUseCase(articleRepository: gh<_i4.EmailRepository>()));
+    gh.factory<_i6.GetAllEmailsUseCase>(() =>
+        _i6.GetAllEmailsUseCase(articleRepository: gh<_i4.EmailRepository>()));
+    gh.factory<_i7.GetMostPopularEmailsUseCase>(() =>
+        _i7.GetMostPopularEmailsUseCase(
+            emailRepository: gh<_i8.EmailRepository>()));
+    gh.factory<_i9.UpdateEmailUseCase>(() =>
+        _i9.UpdateEmailUseCase(articleRepository: gh<_i4.EmailRepository>()));
+    return this;
+  }
+}
